52e45028fd551c45ac4b148b305acce03ce12910
mohamedSalama123 <mohamed3salama33@gmail.com>
2023-01-09 23:43:35 +0200

    add Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
 import 'package:quran_app/screens/home_screen.dart';
 
 void main() {
-  runApp( QuranApp());
+  runApp(QuranApp());
 }
 
 class QuranApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,10 +15,29 @@ class QuranApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      debugShowCheckedModeBanner: false,
       routes: {
-        HomeScreen.routName:(context)=>HomeScreen(),
+        HomeScreen.routName: (context) => HomeScreen(),
       },
-      initialRoute: HomeScreen.routName,
+      home: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(image: AssetImage('assets/images/bg.png')),
+        ),
+        child: AnimatedSplashScreen(
+          backgroundColor: Colors.transparent,
+          duration: 3500,
+          splash: Image.asset(
+            'assets/images/logo.png',
+          ),
+          nextScreen: HomeScreen(),
+          splashTransition: SplashTransition.rotationTransition,
+          pageTransitionType: PageTransitionType.fade,
+          splashIconSize: 250,
+          centered: true,
+        ),
+      ),
     );
   }
 }
